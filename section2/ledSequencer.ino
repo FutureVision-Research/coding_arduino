502b91a44e04417ee1bbc07e9136d3ade3763b78
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2026-02-03 12:29:13 -0800

    Update ledSequencer.ino

--- section2/ledSequencer.ino
+++ section2/ledSequencer.ino
@@ -1,10 +1,5 @@
-/*LED sequencer sketch adapted from theDemonstrator learn to solder board
-
-The setup assumes all LEDS are connected to GPIO pins in sequence.
-
-Original source code available at https://github.com/FutureVision-Research/theDemonstrator
-
-Updated 12/2/2025
+/*LED sequencer sketch 
+Updated 2/3/2026
 by Brian P. Cox, FutureVision Research
 Sketch is under Creative Commmons License Attribution-NonCommercial-ShareAlike 4.0 International
 */
@@ -14,16 +9,16 @@ const int FIRST_LED_PIN = 2, LAST_LED_PIN = 11; // Specifiy the first and last L
 
 // Declare variables
 int sequenceSpeed = 100; //This is the delay between lighting LEDs. Recommend a value between 10 and 1000
-int counter; //used to keep track of counts in for loops
+
 
 void setup() { //Setup function runs once
-  for (counter = FIRST_LED_PIN; counter <= LAST_LED_PIN; counter++) {  //Set all LED pins as outputs
+  for (int counter = FIRST_LED_PIN; counter <= LAST_LED_PIN; counter++) {  //Set all LED pins as outputs
     pinMode(counter,OUTPUT);
     } //End of counter
 }//End of setup() function
 
 void classicSequencer(){ //Lignt one LED at a time from left to right
-  for (counter = FIRST_LED_PIN; counter <= LAST_LED_PIN; ++counter) { //counter used to keep track of LED sequence
+  for(int counter = FIRST_LED_PIN; counter <= LAST_LED_PIN; ++counter) { //counter used to keep track of LED sequence
     digitalWrite(counter, HIGH);
     delay(sequenceSpeed);
     digitalWrite(counter, LOW);
@@ -33,12 +28,12 @@ void classicSequencer(){ //Lignt one LED at a time from left to right
 // This section creates functions for the various types of LED sequencers.
 // We will then call the specific function we want to run. 
 void oneAfterAnother(){ //Light one LED after another then turn each one off
-    for(counter = FIRST_LED_PIN; counter <= LAST_LED_PIN; ++counter) { 
+    for(int counter = FIRST_LED_PIN; counter <= LAST_LED_PIN; ++counter) { 
       delay(sequenceSpeed);
       digitalWrite(counter, HIGH);
       delay(sequenceSpeed);   
     }
-    for(counter = LAST_LED_PIN; counter >= FIRST_LED_PIN; --counter) {
+    for(int counter = LAST_LED_PIN; counter >= FIRST_LED_PIN; --counter) {
       delay(sequenceSpeed);
       digitalWrite(counter, LOW);
       delay(sequenceSpeed);   
@@ -47,26 +42,26 @@ void oneAfterAnother(){ //Light one LED after another then turn each one off
 
 void pingPong(){
   delay(sequenceSpeed);
-  for(counter = FIRST_LED_PIN; counter <= LAST_LED_PIN; ++counter) {//Light one LED at a time from left to right
+  for(int counter = FIRST_LED_PIN; counter <= LAST_LED_PIN; ++counter) {//Light one LED at a time from left to right
     digitalWrite(counter, HIGH);  // turn LED on
-    delay(sequenceSpeed);                    // pause to slow down
+    delay(sequenceSpeed);         // pause to slow down
     digitalWrite(counter, LOW);   // turn LED off
   }
 
   delay(sequenceSpeed);
-  for(counter = LAST_LED_PIN; counter >= FIRST_LED_PIN; --counter) { //Light one LED at a time from right to left
+  for(int counter = LAST_LED_PIN; counter >= FIRST_LED_PIN; --counter) { //Light one LED at a time from right to left
     digitalWrite(counter, HIGH);  // turn LED on
-    delay(sequenceSpeed);                    // pause to slow down
+    delay(sequenceSpeed);         // pause to slow down
     digitalWrite(counter, LOW);   // turn LED off
   }
 }//End of pingPong function
 
 void randomLED(){
   delay(sequenceSpeed);
-  counter = random(FIRST_LED_PIN,LAST_LED_PIN+1);  // pick a LED pin within the proper range
-  digitalWrite(counter, HIGH);  // turn LED on
-  delay(sequenceSpeed);                    // pause to slow down
-  digitalWrite(counter, LOW);   // turn LED off
+  int randomNumber = random(FIRST_LED_PIN,LAST_LED_PIN+1);  // pick a LED pin within the proper range
+  digitalWrite(randomNumber, HIGH);  // turn LED on
+  delay(sequenceSpeed);              // pause to slow down
+  digitalWrite(randomNumber, LOW);   // turn LED off
 }
 
 
@@ -80,7 +75,3 @@ void loop() { //Loop function runs over and over
   //randomLED();
 
 }//End of loop function  
-
-
-
-
