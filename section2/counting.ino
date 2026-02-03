d535a688bcf1777220cfe70d0895e8fe1845a4a9
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2026-02-03 12:17:25 -0800

    Update counting.ino

--- section2/counting.ino
+++ section2/counting.ino
@@ -6,7 +6,7 @@ void setup() {
 void loop() {
   Serial.println("Start at 0 and count to 4");
   for (int i = 0; i < 5; i++) {
-        Serial.println(i);
+    Serial.println(i);
     delay(1000);
   }
 
@@ -14,15 +14,15 @@ void loop() {
 
   Serial.println("Start at 4 and count down to 0");
   for (int i = 4; i>=0; i--) {
-          Serial.println(i);
-     delay(1000);
+    Serial.println(i);
+    delay(1000);
   }  
 
   delay(2000);  // wait 2 seconds before continuing
 
   Serial.println("Start at 0 and count by 2's up to 10");
-    for (int i = 0; i<=10; i+=2) {  // Using i+=2 is the same as saying i = i + 2
-        Serial.println(i);
+  for (int i = 0; i<=10; i+=2) {  // Using i+=2 is the same as saying i = i + 2
+    Serial.println(i);
     delay(1000);
   }  
 
