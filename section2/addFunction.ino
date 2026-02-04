2ac7868da00cbde321d5010ab896dc0b15d7170a
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2026-02-04 09:43:52 -0800

    Create addFunction.ino

---
+++ section2/addFunction.ino
@@ -0,0 +1,16 @@
+// This sketch demonstrates to use use a function
+void setup() {
+  Serial.begin(9600);
+}
+
+int add(int a, int b) {
+  int result = a + b;
+  return result;
+}
+
+void loop() {
+  int answer = add(5,8);
+  Serial.print("The sum of your two numbers is ");
+  Serial.println(answer);
+  delay(5000);
+}
