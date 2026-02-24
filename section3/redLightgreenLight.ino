3a2256e760a4bbf7b6b8649df4c4c2c914972335
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2026-02-24 12:28:04 -0800

    upload red light green light sketch
    This sketch controls a relay for a red light/green light game using an Arduino Uno. It reads a button state to activate or deactivate the relay.

---
+++ section3/redLightgreenLight.ino
@@ -0,0 +1,35 @@
+/* Sketch to control a relay with an Arduino Uno
+ * This can be used to create a basic red light / green light game
+ * The red LED should be connected to Normally Open on the relay
+ * The green LED should be connected to Normally Closed on the relay
+ * Written by Brian Cox
+ * 5/29/2019
+*/
+
+// Precompiler directives are used here to set the pin definitions
+#define BUTTON_PIN 10
+#define RELAY_PIN 9
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the relay pin as an output:
+  pinMode(RELAY_PIN, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(BUTTON_PIN, INPUT);
+  }
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(BUTTON_PIN);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == LOW) {
+    // Activate relay:
+    digitalWrite(RELAY_PIN, HIGH);
+  } else {
+    // Deactivate relay:
+    digitalWrite(RELAY_PIN, LOW);
+  }
+}
