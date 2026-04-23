6f2029550f27b444585f81c9c9371b08b2beab36
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2026-04-23 12:08:18 -0700

    Implement button press functionality for LED control
    Added button functionality to control the built-in LED based on rotary encoder button press.

--- section4/rotaryEncoder.ino
+++ section4/rotaryEncoder.ino
@@ -1,9 +1,11 @@
 // This sketch demonstrates how to receive input from the KY-040 Rotary Encoder
-// This sketch does not receive input from the switch on the KY-040
+// The rotation inforamtion is provided through Serial Monitor
+// When you press the button, the built in LED on the Uno will glow
 
 // Pin connections
 const int clkPin = 2;   // Used for the clock (CLK) pin on the rotary encoder
 const int dtPin  = 3;   // Used for the data (DT) pin on the rotary encoder
+const int buttonPin = 4; // Used for the button/switch (SW) on the rotary encoder
 
 int counter = 0;        // This will change as you turn the knob
 int lastCLKState;       // Used to keep track of the state of CLK
@@ -12,7 +14,11 @@ void setup() {
   // Set both pins as inputs
   pinMode(clkPin, INPUT);
   pinMode(dtPin, INPUT);
-
+  // The button on the rotary controller is grounded when you press it.
+  // Therefore, we need to use a pullup resistor.
+  pinMode(buttonPin, INPUT_PULLUP); 
+  pinMode(LED_BUILTIN, OUTPUT); // Set the built in LED (pin 13) as an output
+  
   Serial.begin(9600);
 
   // Read the starting state of CLK
@@ -20,6 +26,15 @@ void setup() {
 }
 
 void loop() {
+  int buttonPress = digitalRead(buttonPin);
+
+  if (buttonPress == LOW) { //Activate the LED when the button is pressed
+    digitalWrite(LED_BUILTIN, HIGH);
+
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  
   int currentCLKState = digitalRead(clkPin); // Reads the state of the pin for CLK
 
   // Detect a change in the CLK signal
