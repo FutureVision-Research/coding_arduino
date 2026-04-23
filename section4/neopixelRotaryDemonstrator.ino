04fb8e8ab466b672677fba0387a627a5acdc1652
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2026-04-23 12:32:15 -0700

    Improve comments and code structure in NeoPixel demo
    Refactor comments for clarity and improve code readability.

--- section4/neopixelRotaryDemonstrator.ino
+++ section4/neopixelRotaryDemonstrator.ino
@@ -3,41 +3,24 @@
 // --------------------------------------------------
 // NeoPixel settings
 // --------------------------------------------------
-// The NeoPixel data wire is connected to pin 6.
 const int pixelPin = 6;
-
-// We have 5 NeoPixels in the strand.
 const int numberOfPixels = 5;
 
-// Create the NeoPixel object.
-// The strand uses GRB color order and 800 KHz timing.
+// Create the NeoPixel object
 Adafruit_NeoPixel strip(numberOfPixels, pixelPin, NEO_GRB + NEO_KHZ800);
 
 // --------------------------------------------------
 // KY-040 rotary encoder pins
 // --------------------------------------------------
-// CLK and DT are the two encoder output pins.
-// SW is the pushbutton switch built into the encoder.
 const int encoderCLK = 2;
 const int encoderDT  = 3;
 const int encoderSW  = 4;
 
 // --------------------------------------------------
-// Program states
+// enum means enumeration
+// It lets us create a list of named values.
+// This makes the program easier to read.
 // --------------------------------------------------
-/*
-  enum is short for "enumeration."
-
-  An enumeration lets us create a small list of named values.
-
-  In this sketch, we use enum to give names to the three modes
-  of operation. This makes the code easier to read than using
-  plain numbers like 0, 1, and 2.
-
-  IDLE         = the sketch is waiting for the first button press
-  SELECT_PIXEL = the encoder chooses which NeoPixel to edit
-  SELECT_COLOR = the encoder chooses the new color
-*/
 enum Mode
 {
   IDLE,
@@ -45,99 +28,91 @@ enum Mode
   SELECT_COLOR
 };
 
-// This variable stores the current mode.
+// This variable stores the current mode
 Mode currentMode = IDLE;
 
 // --------------------------------------------------
-// Pixel color storage
+// Array to store the saved color of each NeoPixel
 // --------------------------------------------------
-/*
-  Each NeoPixel color is stored as a single number.
-
-  Even though a color is made from red, green, and blue values,
-  the Adafruit library combines those values into one long number.
-
-  We use unsigned long here because it is a classic Arduino type
-  and works well for storing the color value returned by strip.Color().
-*/
 unsigned long pixelColors[numberOfPixels];
 
 // --------------------------------------------------
 // Selection variables
 // --------------------------------------------------
-// Which pixel is currently selected
 int selectedPixel = 0;
-
-// Which color from our list is currently selected
 int selectedColorIndex = 0;
 
-// We want 10 evenly spaced colors around the color wheel
+// Number of available colors
 const int numberOfColors = 10;
 
 // --------------------------------------------------
-// Encoder and button tracking
+// Encoder tracking
 // --------------------------------------------------
-// This stores the previous CLK reading so we can detect change
 int lastCLKState;
 
-// Last reading from the pushbutton
-bool lastButtonState = HIGH;
+// --------------------------------------------------
+// Button variables
+// --------------------------------------------------
+bool buttonReading = HIGH;
+bool lastButtonReading = HIGH;
+bool buttonState = HIGH;
 
-// Used for simple button debouncing
-unsigned long lastButtonChangeTime = 0;
+unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 30;
+
+// These variables are used to detect a long press
+unsigned long buttonPressStartTime = 0;
+bool longPressHandled = false;
+const unsigned long longPressTime = 1000;
 
 // --------------------------------------------------
 // Blink timing
 // --------------------------------------------------
-// blinkState changes from true to false so the selected pixel blinks
 bool blinkState = true;
-
-// Used to measure time between blinks
 unsigned long lastBlinkTime = 0;
 const unsigned long blinkInterval = 300;
 
 // --------------------------------------------------
-// Function declarations
-// --------------------------------------------------
+// Function declarations (prototypes)
+// These list the functions used in this program.
+// The full definitions are written later in the file.
+// A function prototype tells the compiler that a function exists
+// and will be defined later, allowing the function to be used before
+// its full definition appears in the code.
+// -------------------------------------------------------------------------------------------------------------------------------------
 void showAllRed();
 void handleEncoder();
 void handleButton();
 void updateDisplay();
 unsigned long getColorFromIndex(int index);
+void processShortPress();
+void processLongPress();
+int findColorIndex(unsigned long targetColor);
 
 void setup()
 {
-  // Set the encoder pins as inputs
   pinMode(encoderCLK, INPUT);
   pinMode(encoderDT, INPUT);
-
-  // Use the internal pull-up resistor for the button
-  // This means:
-  // not pressed = HIGH
-  // pressed     = LOW
   pinMode(encoderSW, INPUT_PULLUP);
 
-  // Start the NeoPixel strip
   strip.begin();
+  strip.setBrightness(50); //255=full brightness, 10=very dim
   strip.show();
 
-  // Start with all pixels red
   showAllRed();
 
-  // Read the starting state of the CLK pin
   lastCLKState = digitalRead(encoderCLK);
+
+  buttonReading = digitalRead(encoderSW);
+  lastButtonReading = buttonReading;
+  buttonState = buttonReading;
 }
 
 void loop()
 {
-  // Check for encoder movement
   handleEncoder();
-
-  // Check for button presses
   handleButton();
 
-  // If we are in a selection mode, make the selected pixel blink
   if (currentMode == SELECT_PIXEL || currentMode == SELECT_COLOR)
   {
     if (millis() - lastBlinkTime >= blinkInterval)
@@ -150,7 +125,7 @@ void loop()
 }
 
 // --------------------------------------------------
-// Set all NeoPixels to red at the beginning
+// Set all pixels to red
 // --------------------------------------------------
 void showAllRed()
 {
@@ -163,27 +138,21 @@ void showAllRed()
 }
 
 // --------------------------------------------------
-// Read the rotary encoder
+// Read rotary encoder movement
 // --------------------------------------------------
 void handleEncoder()
 {
   int currentCLKState = digitalRead(encoderCLK);
 
-  // We look for a change on CLK.
-  // We only react when CLK becomes HIGH.
-  // This helps reduce extra counts.
   if (currentCLKState != lastCLKState && currentCLKState == HIGH)
   {
-    // Compare DT to CLK to determine direction
     if (digitalRead(encoderDT) != currentCLKState)
     {
       // Clockwise turn
-
       if (currentMode == SELECT_PIXEL)
       {
         selectedPixel++;
 
-        // Wrap around to the beginning
         if (selectedPixel >= numberOfPixels)
         {
           selectedPixel = 0;
@@ -193,7 +162,6 @@ void handleEncoder()
       {
         selectedColorIndex++;
 
-        // Wrap around to the beginning
         if (selectedColorIndex >= numberOfColors)
         {
           selectedColorIndex = 0;
@@ -203,12 +171,10 @@ void handleEncoder()
     else
     {
       // Counterclockwise turn
-
       if (currentMode == SELECT_PIXEL)
       {
         selectedPixel--;
 
-        // Wrap around to the end
         if (selectedPixel < 0)
         {
           selectedPixel = numberOfPixels - 1;
@@ -218,7 +184,6 @@ void handleEncoder()
       {
         selectedColorIndex--;
 
-        // Wrap around to the end
         if (selectedColorIndex < 0)
         {
           selectedColorIndex = numberOfColors - 1;
@@ -229,63 +194,107 @@ void handleEncoder()
     updateDisplay();
   }
 
-  // Save this reading for next time through the loop
   lastCLKState = currentCLKState;
 }
 
 // --------------------------------------------------
-// Read the pushbutton on the encoder
+// Read and debounce the pushbutton
+// Also detect short press and long press
 // --------------------------------------------------
 void handleButton()
 {
-  bool currentButtonState = digitalRead(encoderSW);
+  buttonReading = digitalRead(encoderSW);
 
-  // If the button reading changed, reset the debounce timer
-  if (currentButtonState != lastButtonState)
+  if (buttonReading != lastButtonReading)
   {
-    lastButtonChangeTime = millis();
+    lastDebounceTime = millis();
   }
 
-  // Wait a short time so button bouncing settles down
-  if (millis() - lastButtonChangeTime > debounceDelay)
+  if ((millis() - lastDebounceTime) > debounceDelay)
   {
-    // Detect a button press: HIGH to LOW
-    if (lastButtonState == HIGH && currentButtonState == LOW)
+    if (buttonReading != buttonState)
     {
-      if (currentMode == IDLE)
-      {
-        // First click:
-        // start selecting pixels
-        selectedPixel = 0;
-        currentMode = SELECT_PIXEL;
-        blinkState = true;
-        updateDisplay();
-      }
-      else if (currentMode == SELECT_PIXEL)
-      {
-        // Second click:
-        // choose a color for the blinking pixel
-        currentMode = SELECT_COLOR;
-        selectedColorIndex = 0;
-        blinkState = true;
-        updateDisplay();
-      }
-      else if (currentMode == SELECT_COLOR)
-      {
-        // Third click:
-        // save the chosen color into the selected pixel
-        pixelColors[selectedPixel] = getColorFromIndex(selectedColorIndex);
+      buttonState = buttonReading;
 
-        // Return to pixel selection mode
-        currentMode = SELECT_PIXEL;
-        blinkState = true;
-        updateDisplay();
+      // Button has just been pressed
+      if (buttonState == LOW)
+      {
+        buttonPressStartTime = millis();
+        longPressHandled = false;
+      }
+
+      // Button has just been released
+      if (buttonState == HIGH)
+      {
+        if (!longPressHandled)
+        {
+          processShortPress();
+        }
       }
     }
   }
 
-  // Save this reading for next time through the loop
-  lastButtonState = currentButtonState;
+  if (buttonState == LOW && !longPressHandled)
+  {
+    if (millis() - buttonPressStartTime >= longPressTime)
+    {
+      processLongPress();
+      longPressHandled = true;
+    }
+  }
+
+  lastButtonReading = buttonReading;
+}
+
+// --------------------------------------------------
+// Handle a normal short press
+// --------------------------------------------------
+void processShortPress()
+{
+  if (currentMode == IDLE)
+  {
+    // First short press enters pixel selection mode
+    selectedPixel = 0;
+    currentMode = SELECT_PIXEL;
+    blinkState = true;
+    updateDisplay();
+  }
+  else if (currentMode == SELECT_PIXEL)
+  {
+    // Next short press enters color selection mode
+    currentMode = SELECT_COLOR;
+
+    // Start from this pixel's current saved color
+    selectedColorIndex = findColorIndex(pixelColors[selectedPixel]);
+
+    blinkState = true;
+    updateDisplay();
+  }
+  else if (currentMode == SELECT_COLOR)
+  {
+    // Save the chosen color to the selected pixel
+    pixelColors[selectedPixel] = getColorFromIndex(selectedColorIndex);
+
+    // Return to pixel selection mode
+    currentMode = SELECT_PIXEL;
+    blinkState = true;
+    updateDisplay();
+  }
+}
+
+// --------------------------------------------------
+// Handle a long press
+// --------------------------------------------------
+void processLongPress()
+{
+  if (currentMode == SELECT_COLOR)
+  {
+    pixelColors[selectedPixel] = getColorFromIndex(selectedColorIndex);
+  }
+
+  currentMode = IDLE;
+  blinkState = true;
+  updateDisplay();
 }
 
 // --------------------------------------------------
@@ -293,24 +302,31 @@ void handleButton()
 // --------------------------------------------------
 unsigned long getColorFromIndex(int index)
 {
-  /*
-    ColorHSV() creates colors using hue.
-
-    Hue is the position around the color wheel.
-    We divide the wheel into 10 equal parts.
-
-    gamma32() helps the colors look more natural to our eyes.
-  */
   unsigned long hue = (65535UL * index) / numberOfColors;
   return strip.gamma32(strip.ColorHSV(hue));
 }
 
 // --------------------------------------------------
-// Update what the NeoPixels display
+// Find which color index matches a saved pixel color
+// --------------------------------------------------
+int findColorIndex(unsigned long targetColor)
+{
+  for (int i = 0; i < numberOfColors; i++)
+  {
+    if (getColorFromIndex(i) == targetColor)
+    {
+      return i;
+    }
+  }
+
+  return 0;
+}
+
+// --------------------------------------------------
+// Update the NeoPixel display
 // --------------------------------------------------
 void updateDisplay()
 {
-  // First, show all saved pixel colors
   for (int i = 0; i < numberOfPixels; i++)
   {
     strip.setPixelColor(i, pixelColors[i]);
@@ -318,8 +334,6 @@ void updateDisplay()
 
   if (currentMode == SELECT_PIXEL)
   {
-    // In pixel selection mode,
-    // the selected pixel blinks on and off
     if (!blinkState)
     {
       strip.setPixelColor(selectedPixel, 0);
@@ -327,8 +341,6 @@ void updateDisplay()
   }
   else if (currentMode == SELECT_COLOR)
   {
-    // In color selection mode,
-    // the selected pixel blinks using the preview color
     if (blinkState)
     {
       strip.setPixelColor(selectedPixel, getColorFromIndex(selectedColorIndex));
